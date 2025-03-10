d13377d5794d6491f7913bf39409753e6d441263
Malcret <theotime.marcopoulos@malcret.com>
2025-03-10 17:08:42 +0100

    fix(deploy): uncomment condition and remove ln `f` argument

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ deploy() {
     if [ -d $cwd/$config ] || [ -e $cwd/$config ]; then
         local path="$HOME/.$config"
         mkdir -p $(dirname $path)
-        # if [ ! -d $path ] && [ ! -e $path ] && [ ! -L $path ]; then
-        echo "Deploy '$config'..."
-        ln -sf $cwd/$config $path
-        # else
-        echo "Skip, '$path' already exist!"
-        # fi
+        if [ ! -d $path ] && [ ! -e $path ] && [ ! -L $path ]; then
+            echo "Deploy '$config'..."
+            ln -s $cwd/$config $path
+        else
+            echo "Skip, '$path' already exist!"
+        fi
     else
         echo "Invalid config, '$config' doesn't exist!"
     fi
